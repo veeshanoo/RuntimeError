e18d2bfb5fbd8e502a647e131dd1c11f48fa68ff
Mihai Vlad <mihaivlad230299@gmail.com>
2022-12-29 19:11:44 +0200

    Question changes

--- db/mongo/question.go
+++ db/mongo/question.go
@@ -3,6 +3,8 @@ package mongo
 import (
 	types "RuntimeError/types/mongo"
 	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 const questionsCollectionName = "questions"
@@ -21,4 +23,30 @@ func (q *QuestionRepoImpl) Insert(ctx context.Context, question *types.Question)
 	question.Downvoters = nil
 
 	return Insert(ctx, questionsCollectionName, question, QuestionLabel)
+}
+
+func (q *QuestionRepoImpl) Update(ctx context.Context, oldQuestion *types.Question, newQuestion *types.Question) (*types.Question, error) {
+	return nil, nil
+}
+
+func (q *QuestionRepoImpl) Delete(ctx context.Context, id string) error {
+	return nil
+}
+
+func (u *QuestionRepoImpl) GetAll(ctx context.Context) ([]*types.Question, error) {
+	result, err := GetAll(ctx, questionsCollectionName, bson.M{}, QuestionLabel)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.([]*types.Question), nil
+}
+
+func (u *QuestionRepoImpl) GetById(ctx context.Context, id string) (*types.Question, error) {
+	result, err := GetOne(ctx, questionsCollectionName, bson.M{"_id": id}, QuestionLabel)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*types.Question), nil
 }
