2f9db2b552cb7b15b8efec4d0565123042d69ae0
Mihai Vlad <mihaivlad230299@gmail.com>
2022-12-29 20:01:08 +0200

    Question mongo data changes

--- db/mongo/question.go
+++ db/mongo/question.go
@@ -50,3 +50,53 @@ func (u *QuestionRepoImpl) GetById(ctx context.Context, id string) (*types.Quest
 
 	return result.(*types.Question), nil
 }
+
+func (u *QuestionRepoImpl) UpvoteQuestion(ctx context.Context, id string, upvotterId string) (*types.Question, error) {
+	result, err := GetOne(ctx, questionsCollectionName, bson.M{"_id": id}, QuestionLabel)
+	if err != nil {
+		return nil, err
+	}
+	question := result.(*types.Question)
+	question.Upvoters = append(question.Upvoters, upvotterId)
+	return u.Update(ctx, result.(*types.Question), question)
+}
+
+func (u *QuestionRepoImpl) DownvoteQuestion(ctx context.Context, id string, downvotterId string) (*types.Question, error) {
+	result, err := GetOne(ctx, questionsCollectionName, bson.M{"_id": id}, QuestionLabel)
+	if err != nil {
+		return nil, err
+	}
+	question := result.(*types.Question)
+	question.Downvoters = append(question.Upvoters, downvotterId)
+	return u.Update(ctx, result.(*types.Question), question)
+}
+
+func (u *QuestionRepoImpl) AddAnswerToQuestion(ctx context.Context, id string, answer *types.Answer) (*types.Question, error) {
+	result, err := GetOne(ctx, questionsCollectionName, bson.M{"_id": id}, QuestionLabel)
+	if err != nil {
+		return nil, err
+	}
+	question := result.(*types.Question)
+	question.Answers = append(question.Answers, *answer)
+	return u.Update(ctx, result.(*types.Question), question)
+}
+
+func (u *QuestionRepoImpl) EditContent(ctx context.Context, id string, newContent string) (*types.Question, error) {
+	result, err := GetOne(ctx, questionsCollectionName, bson.M{"_id": id}, QuestionLabel)
+	if err != nil {
+		return nil, err
+	}
+	question := result.(*types.Question)
+	question.Contents = newContent
+	return u.Update(ctx, result.(*types.Question), question)
+}
+
+func (u *QuestionRepoImpl) FavoriteComment(ctx context.Context, id string, bestAnswer string) (*types.Question, error) {
+	result, err := GetOne(ctx, questionsCollectionName, bson.M{"_id": id}, QuestionLabel)
+	if err != nil {
+		return nil, err
+	}
+	question := result.(*types.Question)
+	question.BestAnswer = bestAnswer
+	return u.Update(ctx, result.(*types.Question), question)
+}
--- repo/question.go
+++ repo/question.go
@@ -6,8 +6,9 @@ import (
 )
 
 type QuestionRepo interface {
-	// GetAll(ctx context.Context) ([]*types.Question, error)
-	// GetById(ctx context.Context, id string) (*types.Question, error)
-	Insert(ctx context.Context, user *types.Question) (string, error)
-	// Update(ctx context.Context, oldQ *types.Question, newQ *types.Question) (*types.Question, error)
+	GetAll(ctx context.Context) ([]*types.Question, error)
+	GetById(ctx context.Context, id string) (*types.Question, error)
+	Insert(ctx context.Context, question *types.Question) (string, error)
+	Update(ctx context.Context, oldQ *types.Question, newQ *types.Question) (*types.Question, error)
+	Delete(ctx context.Context, id string) error
 }
